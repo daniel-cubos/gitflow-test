03c9e2ae6da8f82f3a3ab75cee39fc97022cdfbe
danieldeandradelopes <danieldeandradelopes@gmail.com>
2021-07-20 15:41:08 -0300

    create list of products

---
+++ productsController.js
@@ -0,0 +1,17 @@
+const products = [
+  {
+    id: 1,
+    name: "Coca cola",
+    category: "Refrigerante"
+  },
+  {
+    id: 2,
+    name: "Guaraná Antártica",
+    category: "Refrigerante"
+  },
+  {
+    id: 1,
+    name: "Leite Moça",
+    category: "Leite condensado"
+  }
+];
